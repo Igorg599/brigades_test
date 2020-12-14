e06ea65747afa6b37697c3b52cfed91893843b44
Igorg599 <igorg599@gmail.com>
2020-12-14 19:36:21 +0500

    commit-Heroku

--- src/services/brigades-services.js
+++ src/services/brigades-services.js
@@ -1,7 +1,8 @@
 export default class GetBrigades{
+    _apiBase = 'http://localhost:3000';
 
     async getResource(url) {
-        const res = await fetch(`${url}`);
+        const res = await fetch(`${this._apiBase}${url}`);
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}` + 
                 `, received ${res.status}`);
